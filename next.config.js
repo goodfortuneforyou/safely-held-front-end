6362d562e6f13ee278f72e61e1644c44dfba4622
DulquerSumon <m.dulquer.sumon@gmail.com>
2023-10-26 00:35:45 +0600

    next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ["ipfs.io"],
+    loader: "akamai",
+    path: "",
+    unoptimized: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
