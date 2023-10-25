b66423a1f9b4f3c66964bd653898c80e7de3348f
DulquerSumon <m.dulquer.sumon@gmail.com>
2023-10-26 01:00:42 +0600

    Nav bar

---
+++ components/Navbar.js
@@ -0,0 +1,111 @@
+import { useContext, useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import { useTheme } from "next-themes";
+import Image from "next/image";
+import Link from "next/link";
+import { NFTContext } from "../context/NFTContext";
+import images from "../assets";
+// eslint-disable-next-line import/no-cycle
+import { Button } from ".";
+
+const Navbar = () => {
+  const { theme, setTheme } = useTheme();
+  const [active, setActive] = useState("Explore NFTs");
+  const [isOpen, setIsOpen] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    setTheme("dark");
+  }, []);
+
+  return (
+    <nav className="flexBetween w-full fixed z-10 p-4 flex-row border-b dark:bg-nft-dark bg-white dark:border-nft-black-1 border-nft-gray-1">
+      <div className="flex flex-1 flex-row justify-start">
+        <Link href="/">
+          <div
+            className="flexCenter md:hidden cursor-pointer"
+            onClick={() => {
+              setActive("Explore NFTs");
+            }}
+          >
+            <Image
+              src={images.logo02}
+              objectFit="contain"
+              width={32}
+              height={32}
+              alt="logo"
+            />
+            <p className="dark:text-white text-nft-black-1 font-semibold text-lg ml-1">
+              SafelyHeld
+            </p>
+          </div>
+        </Link>
+        <Link href="/">
+          <div
+            className="hidden md:flex"
+            onClick={() => {
+              setActive("Explore NFTs");
+              setIsOpen(false);
+            }}
+          >
+            <Image
+              src={images.logo02}
+              objectFit="contain"
+              width={32}
+              height={32}
+              alt="logo"
+            />
+          </div>
+        </Link>
+      </div>
+      <div className="flex flex-initial flex-row justify-end">
+        <div className="flex items-center mr-2">
+          <input
+            type="checkbox"
+            className="checkbox"
+            id="checkbox"
+            onChange={() => setTheme(theme === "light" ? "dark" : "light")}
+          />
+          <label
+            htmlFor="checkbox"
+            className="flexBetween w-8 h-4 bg-black rounded-2xl p-1 relative label"
+          >
+            <i className="fas fa-sun" />
+            <i className="fas fa-moon" />
+            <div className="w-3 h-3 absolute bg-white rounded-full ball" />
+          </label>
+        </div>
+
+        <div className="md:hidden flex " />
+      </div>
+      <div className="hidden md:flex ml-2">
+        {isOpen ? (
+          <Image
+            src={images.cross}
+            objectFit="contain"
+            width={20}
+            height={20}
+            alt="close"
+            onClick={() => setIsOpen(false)}
+            className={theme === "light" ? "filter invert" : ""}
+          />
+        ) : (
+          <Image
+            src={images.menu}
+            objectFit="contain"
+            width={25}
+            height={25}
+            alt="menu"
+            onClick={() => setIsOpen(true)}
+            className={theme === "light" ? "filter invert" : ""}
+          />
+        )}
+        {isOpen && (
+          <div className="fixed inset-0 top-65 dark:bg-nft-dark bg-white z-10 nav-h flex justify-between flex-col" />
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
